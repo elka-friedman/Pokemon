b301cf62a9859d894a132e0ba79d94f42e7a1e63
Elka Friedman <elkafriedman1@gmail.com>
2023-02-05 13:22:35 -0500

    useEffect

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,14 @@
-import React, {useState} from "react";
+import React, {useState , useEffect} from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0)
+    useEffect(() => {
+        document.title= `you clicked ${count} times`
+    })
     console.log('Count on state', count);
     return (
         <div> 
-            <p>You clicked {count} time </p>
+            <p>You clicked {count} times </p>
             <button onClick={() => setCount(count +1)}>
                 Click to increment counter
             </button>
