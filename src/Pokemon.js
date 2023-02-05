7aac9c2b813c8a7d58fb45e7e6b7cdbd99dd7859
Elka Friedman <elkafriedman1@gmail.com>
2023-02-05 12:21:32 -0500

    commit

---
+++ src/Pokemon.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const Pokemon = () => {
+//     try{
+//   let pokemon = axios
+//     .get("https://pokeapi.co/api/v2/pokemon")
+//     .then((response) => {
+//       console.log(response.data);
+//     });
+// } catch (err) {
+//     console.log(err)
+// }
+
+const getPokeman = async () => {
+    try {
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+  return (
+    <>
+      {/* {pokemon.response.map((pokemon) => {
+        return <p>{pokemon.name}</p>;
+      })} */}
+    </>
+  );
+};
+
+export default Pokemon;
