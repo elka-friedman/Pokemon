b42252ed842ce71a5b8ae7efc8162337fb2c73d3
Elka Friedman <elkafriedman1@gmail.com>
2023-02-05 12:50:51 -0500

    pokedata with async

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,30 +1,31 @@
 import axios from "axios";
 
 const Pokemon = () => {
-//     try{
-//   let pokemon = axios
-//     .get("https://pokeapi.co/api/v2/pokemon")
-//     .then((response) => {
-//       console.log(response.data);
-//     });
-// } catch (err) {
-//     console.log(err)
-// }
+  //     try{
+  //   let pokemon = axios
+  //     .get("https://pokeapi.co/api/v2/pokemon")
+  //     .then((response) => {
+  //       console.log(response.data);
+  //     });
+  // } catch (err) {
+  //     console.log(err)
+  // }
 
-const getPokeman = async () => {
+  const getPokeman = async () => {
     try {
-
+      const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
+      console.log(data);
     } catch (err) {
-        console.log(err);
+      console.log(err);
     }
-};
-
+  };
 
   return (
     <>
       {/* {pokemon.response.map((pokemon) => {
         return <p>{pokemon.name}</p>;
       })} */}
+      <button onClick={getPokeman}>Pokemon</button>
     </>
   );
 };
